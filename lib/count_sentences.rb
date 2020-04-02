9e414b64f2b6c6dfee7be896f82d2f7a1bbb2226
Shaina C <scailles23@gmail.com>
2020-04-02 04:34:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,10 +20,15 @@ class String
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    self.split
 
   end
   
